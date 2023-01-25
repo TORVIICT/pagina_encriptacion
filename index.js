87524aa1fd8b06f492f3f0192da61ee9a2524f26
victor realpe <victormanuelrealpepopayan@gmail.com>
2023-01-25 01:03:41 -0500

    boton hecho

---
+++ index.js
@@ -0,0 +1,27 @@
+function descencriptar() {
+    
+    var x = document.getElementById('contenido');
+    if (x.style.display === 'none') {
+      x.style.display = 'block';
+    } else {
+        var texto = document.getElementById('texto').value;
+        x.innerHTML = texto;
+    }
+  };
+//
+function encriptar() {
+
+    var texto = document.getElementById('texto').value;
+
+    a=0
+
+    if (x=texto, a=0, x++ ){
+
+        if (x = e){
+            x="enter"
+        }
+
+    }
+
+
+}
